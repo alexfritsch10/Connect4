fe38c5e3ad617a7d069369c2e09503f1536becdf
Alexander Fritsch <alexfritsch10@gmail.com>
2021-01-08 20:25:59 +0100

    Add alpha beta pruning and debug code

--- agents/agent_minimax/gameState.py
+++ agents/agent_minimax/gameState.py
@@ -10,16 +10,12 @@ class GameState:
         """
         recursively building game tree, every middle node (those who are less than depth 4) has list of children,
         building when GameState with root is called. All nodes on depth 4 get a board copy, it'll get modified later by
-        buildGameStateFromID and the value of all nodes is None at the beginning.
+        buildGameStateFromID and the score of all nodes is None at the beginning.
         """
         self.positionID = positionID
         self.player = player
-        self.status = None                 # added self status in case a combination of moves is impossible
-        self.value = None
-        #if positionID == '':
-        #    self.bmove = None
-        # if board is not None:
-        #    self.gameState = board.copy()  # use board.copy() anstatt board, sonst machst du alle changes auf dem gleichen board
+        self.status = None                 # added self.status in case a combination of moves is impossible
+        self.score = None
         if len(positionID) < 4:
             # print('positionID <4:', positionID)
             self.children = []
@@ -28,39 +24,26 @@ class GameState:
         else:
             # print('positionID >= 4: ', positionID)
             self.board = board.copy()
-            #if positionID == '6666':
-            #    print('board: ', pretty_print_board(self.board))
 
-        # besser: if positionID.len < 4:
-        #           children
-        #         else:
-        #           self.board = board
-
-  #  def computeScore(self):
-  #      if connected_four(self.gameState, self.player):
-  #          return 100
-  #      elif column_to_be_played_for_win(self.gameState, self.player) != -1:
-  #          return -100
-  #      else:
-  #          return 0
-
-    def set_value(self):
+    def compute_score(self):
         """
-        uses heuristic to set value of node,
+        uses heuristic to set score of node,
         for loop to find the best column according to heuristic makes it very slow 
         """
-        maxim = -np.Inf
+        self.score = -1000
         for i in range(7):
             if move_is_possible(self.board, i):
                 current = evaluate_heuristic(self.board, i, self.player)
-                if current > maxim:
-                    maxim = current
-                    self.value = i
+                if current > self.score:
+                    self.score = current
 
-        if self.value is None:
+        #print('Set score ' + str(self.score) + ' for node ' + self.positionID + ' with board ' + pretty_print_board(self.board))
+
+        if self.score == -1000:
             self.status = 'impossible'
-            print('no moves for Node:')
+            print('no possible moves for Node:')
             print(self.positionID)
+
 
     def buildGameStateFromID(self):
         """
@@ -75,6 +58,7 @@ class GameState:
             else:
                 self.status = 'impossible'
                 break
+        # print(pretty_print_board(self.board))
         # hier hast du den player am anfang auf 1 gesetzt, egal ob player 1 oder player 2 spielt
 
 
